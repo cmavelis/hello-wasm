f5d4b5386b6b052e0e8d3f968396b2347aefc8eb
Cameron Avelis <AvelisC@michigan.gov>
2024-04-19 00:19:02 -0700

    change to sum function

--- counter.js
+++ counter.js
@@ -1,9 +1,17 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+export function setupCounter(input1, input2, sumDisplayElement) {
+  let number1 = 0;
+  let number2 = 0;
+  const setSum = () => {
+    const sum = number1 + number2;
+    sumDisplayElement.innerHTML = `sum is ${sum}`;
+  };
+
+  input1.addEventListener("input", (e) => {
+    number1 = Number(e.target.value);
+    setSum();
+  });
+  input2.addEventListener("input", (e) => {
+    number2 = Number(e.target.value);
+    setSum();
+  });
 }
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -12,13 +12,14 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <input id="number1" type="number" />
+    <input id="number2" type="number" />
+    <p id="sum">Type numbers in the inputs to sum them</p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(
+  document.querySelector("#number1"),
+  document.querySelector("#number2"),
+  document.querySelector("#sum"),
+);
